c457691731eebe165d1314c05f92e4b40138114e
dev <dev@LAB24-PC>
2022-05-26 15:12:41 -0300

    Adicionar arquivos de projeto.

---
+++ JogoVelha/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace JogoVelha
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.lblP1 = new System.Windows.Forms.Label();
+            this.lblP2 = new System.Windows.Forms.Label();
+            this.lblEmpate = new System.Windows.Forms.Label();
+            this.lblNada1 = new System.Windows.Forms.Label();
+            this.lblNada2 = new System.Windows.Forms.Label();
+            this.lblNada3 = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(7, 17);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 71);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(98, 17);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(85, 71);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(189, 17);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(85, 71);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(7, 94);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(85, 71);
+            this.button4.TabIndex = 3;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(98, 94);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(85, 71);
+            this.button5.TabIndex = 4;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(189, 94);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(85, 71);
+            this.button6.TabIndex = 5;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(7, 171);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(85, 71);
+            this.button7.TabIndex = 6;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(98, 171);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(85, 71);
+            this.button8.TabIndex = 7;
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(189, 171);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(85, 71);
+            this.button9.TabIndex = 8;
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // lblP1
+            // 
+            this.lblP1.AutoSize = true;
+            this.lblP1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblP1.Location = new System.Drawing.Point(7, 255);
+            this.lblP1.Name = "lblP1";
+            this.lblP1.Size = new System.Drawing.Size(71, 16);
+            this.lblP1.TabIndex = 9;
+            this.lblP1.Text = "P1 Vítorias";
+            // 
+            // lblP2
+            // 
+            this.lblP2.AutoSize = true;
+            this.lblP2.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblP2.Location = new System.Drawing.Point(98, 255);
+            this.lblP2.Name = "lblP2";
+            this.lblP2.Size = new System.Drawing.Size(71, 16);
+            this.lblP2.TabIndex = 10;
+            this.lblP2.Text = "P2 Vítorias";
+            // 
+            // lblEmpate
+            // 
+            this.lblEmpate.AutoSize = true;
+            this.lblEmpate.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblEmpate.Location = new System.Drawing.Point(189, 255);
+            this.lblEmpate.Name = "lblEmpate";
+            this.lblEmpate.Size = new System.Drawing.Size(52, 16);
+            this.lblEmpate.TabIndex = 11;
+            this.lblEmpate.Text = "Empate";
+            // 
+            // lblNada1
+            // 
+            this.lblNada1.AutoSize = true;
+            this.lblNada1.Location = new System.Drawing.Point(14, 292);
+            this.lblNada1.Name = "lblNada1";
+            this.lblNada1.Size = new System.Drawing.Size(40, 15);
+            this.lblNada1.TabIndex = 12;
+            this.lblNada1.Text = "NADA";
+            // 
+            // lblNada2
+            // 
+            this.lblNada2.AutoSize = true;
+            this.lblNada2.Location = new System.Drawing.Point(98, 292);
+            this.lblNada2.Name = "lblNada2";
+            this.lblNada2.Size = new System.Drawing.Size(40, 15);
+            this.lblNada2.TabIndex = 13;
+            this.lblNada2.Text = "NADA";
+            // 
+            // lblNada3
+            // 
+            this.lblNada3.AutoSize = true;
+            this.lblNada3.Location = new System.Drawing.Point(189, 292);
+            this.lblNada3.Name = "lblNada3";
+            this.lblNada3.Size = new System.Drawing.Size(40, 15);
+            this.lblNada3.TabIndex = 14;
+            this.lblNada3.Text = "NADA";
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(84, 319);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(85, 33);
+            this.btnReset.TabIndex = 15;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(272, 354);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.lblNada3);
+            this.Controls.Add(this.lblNada2);
+            this.Controls.Add(this.lblNada1);
+            this.Controls.Add(this.lblEmpate);
+            this.Controls.Add(this.lblP2);
+            this.Controls.Add(this.lblP1);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Label lblP1;
+        private Label lblP2;
+        private Label lblEmpate;
+        private Label lblNada1;
+        private Label lblNada2;
+        private Label lblNada3;
+        private Button btnReset;
+    }
+}
---
+++ JogoVelha/Global.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JogoVelha
+{
+    public class Global
+    {      
+            public static bool turn, button_disable;
+        public static int player1_wins = 0, player2_wins = 0, tie = 0, rounds = 0, A = 0, B = 0, C = 0, D = 0, E = 0, F = 0, G = 0, H = 0, I = 0;
+       
+       
+    }
+   
+}
